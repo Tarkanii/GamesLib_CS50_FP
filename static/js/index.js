4d9978543f272b4bcc7992de5afc21e04a307502
Tarkanii Artur <arturmandragon@gmail.com>
2024-10-26 10:17:18 +0100

    FL-11, implement my-library functionality

--- static/js/index.js
+++ static/js/index.js
@@ -1,17 +1,13 @@
 import { loginHandler } from "./login.js";
 import { signupHandler } from "./signup.js";
-import { burgerMenuHandler, modalCloseButtonHandler, readMoreButtonHandler, backButtonHandler, paginationButtonHandler } from "./buttonHandlers.js";
 import { searchSubmitHandler } from "./search.js";
 import { setPrevUrl } from "./setPreviousUrl.js";
+import { buttonsHandler } from "./buttonHandlers.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-    burgerMenuHandler();
-    modalCloseButtonHandler();
     loginHandler();
     signupHandler();
-    readMoreButtonHandler();
     searchSubmitHandler();
-    backButtonHandler();
-    paginationButtonHandler();
     setPrevUrl();
+    buttonsHandler();
 })
